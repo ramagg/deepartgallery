694e4ca52ee5836227617fabb23022ff6dc089a1
ramagg <rg1998@hotmail.es>
2020-03-17 15:41:35 -0300

    build

---
+++ build/precache-manifest.2176d94850a40159341f05bc6a5b2706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f144c35099e3ae913719d9b8397d26c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a4088f755e6a84426b",
+    "url": "/static/js/2.ab5cbfb3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ab5cbfb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c601ef91281b48a8ed73",
+    "url": "/static/js/main.d7e9a2fc.chunk.js"
+  },
+  {
+    "revision": "3736c26de5f90f7c716e",
+    "url": "/static/js/runtime-main.8a6a57ae.js"
+  }
+]);
---
+++ build/static/js/main.d7e9a2fc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdeepartgallery=this.webpackJsonpdeepartgallery||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),l=n(2);var c=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null," DeepArtGallery "))};n.n(l).a.render(a.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.d7e9a2fc.chunk.js.map
