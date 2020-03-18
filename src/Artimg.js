9d16f6469fef9640be74d402171023e93ccef639
ramagg <rg1998@hotmail.es>
2020-03-18 18:19:43 -0300

    Image fetching

--- src/Artimg.js
+++ src/Artimg.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Artimg(props) {
 
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(false)
   const [details, setDetails] = useState(false)
+  const [rawData, setRawData] = useState(null)
 
+  useEffect(() => {
+    fetch(props.url).then((response) => {
+      response.arrayBuffer().then((buffer) => {
+        var base64Flag = 'data:image/jpeg;base64,';
+        var imageStr = arrayBufferToBase64(buffer);
+        setRawData(base64Flag + imageStr);
+        setShow(true)
+        // console.log(base64Flag + imageStr)
+      });
+    });
+    function arrayBufferToBase64(buffer) {
+      var binary = '';
+      var bytes = [].slice.call(new Uint8Array(buffer));
+
+      bytes.forEach((b) => binary += String.fromCharCode(b));
+
+      return window.btoa(binary);
+    };
+  }, [details])
   return (
     <div className={show ? "imgContainer" : "hide"}>
       {/* <img src={props.src} alt={props.descr} onLoad={setShow(true)} /> */}
-      <img src={props.src} alt={props.descr} />
+      <img src={rawData} alt={props.descr} />
       <div className="actions">
         <span className="details" onClick={() => setDetails(x => !x)}>Details</span>
         <span className="imgDwl" onClick={() => window.location = props.src}>Download HD</span>
