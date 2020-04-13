b6baa4b42a265e94ff005bd90d58e3611a92b13e
ramagg <rg1998@hotmail.es>
2020-04-12 21:44:37 -0300

    image loading change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import ImageColumn from './ImageColumn'
 
-let data = { "imgs": [{ "src": "Fox-colorful-Filia-.jpg", "title": "Fox", "style": "colorful", "original": "Filia" }, { "src": "mclaren-paiting-pablo-.jpg", "title": "mclaren", "style": "paiting", "original": "pablo" }, { "src": "moto-colorful-Clara-.jpg", "title": "moto", "style": "colorful", "original": "Clara" }, { "src": "title-style-orignal- (10).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (11).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (12).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (13).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (2).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (3).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (4).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (5).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (6).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (7).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (8).jpg", "title": "title", "style": "style", "original": "orignal" }, { "src": "title-style-orignal- (9).jpg", "title": "title", "style": "style", "original": "orignal" }] }
+const grupArr = arr => {
+  let startArr = arr
+  let lenSA = startArr.length
+  let grupedArr = []
+  let columns = 4
 
-const grupArr = (arr) => {
-  let startArr = arr;
-  let lenSA = startArr.length;
-  let grupedArr = [];
-  let columns = 4;
-
-  let div = parseInt(lenSA / columns);
+  let div = parseInt(lenSA / columns)
   for (let i = 0; i < columns; i++) {
-    let temp = startArr.slice(i * div, (i + 1) * div);
+    let temp = startArr.slice(i * div, (i + 1) * div)
     grupedArr.push(temp)
   }
-  return grupedArr;
+  return grupedArr
 }
 
 function App() {
-  const [dataurl, setdataurl] = useState({ "imgs": [] })
+  const [dataurl, setdataurl] = useState({ imgs: [] })
 
   const getData = () => {
-
-    fetch('https://ramagg.com/deepartgallery/imgs/').then((response) => response.json())
-      .then((data) => {
-        console.log("Fetching")
-        setdataurl(data);
-      });
+    fetch('https://ramagg.com/deepartgallery/imgs/')
+      .then(response => response.json())
+      .then(data => {
+        setdataurl(data)
+      })
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
   let grupedArr = grupArr(dataurl.imgs)
-
-
 
   return (
     <div className="App">
@@ -42,14 +37,12 @@ function App() {
         <h1>DeepArtGallery</h1>
       </header>
       <div className="artBox">
-        {
-          grupedArr.map((item, i) => {
-            return <ImageColumn key={i} idx={i} imgs={item} />
-          })
-        }
+        {grupedArr.map((item, i) => {
+          return <ImageColumn key={i} idx={i} imgs={item} />
+        })}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Artimg.js
+++ src/Artimg.js
@@ -1,39 +1,42 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Artimg(props) {
-
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(false)
   const [details, setDetails] = useState(false)
   const [rawData, setRawData] = useState(null)
 
   useEffect(() => {
-    // fetch(props.url).then((response) => {
-    //   response.arrayBuffer().then((buffer) => {
-    //     var base64Flag = 'data:image/jpeg;base64,';
-    //     var imageStr = arrayBufferToBase64(buffer);
-    //     setRawData(base64Flag + imageStr);
-    //     setShow(true)
-    //     // console.log(base64Flag + imageStr)
-    //   });
-    // });
-    // function arrayBufferToBase64(buffer) {
-    //   var binary = '';
-    //   var bytes = [].slice.call(new Uint8Array(buffer));
+    fetch(props.url).then(response => {
+      response.arrayBuffer().then(buffer => {
+        var base64Flag = 'data:image/jpeg;base64,'
+        var imageStr = arrayBufferToBase64(buffer)
+        setRawData(base64Flag + imageStr)
+        setShow(true)
+        console.log(base64Flag + imageStr)
+      })
+    })
+    function arrayBufferToBase64(buffer) {
+      var binary = ''
+      var bytes = [].slice.call(new Uint8Array(buffer))
 
-    //   bytes.forEach((b) => binary += String.fromCharCode(b));
+      bytes.forEach(b => (binary += String.fromCharCode(b)))
 
-    //   return window.btoa(binary);
-    // };
-  }, [details])
+      return window.btoa(binary)
+    }
+  }, [details, rawData])
   return (
-    <div className={show ? "imgContainer" : "hide"}>
+    <div className={show ? 'imgContainer' : 'hide'}>
       {/* <img src={props.src} alt={props.descr} onLoad={setShow(true)} /> */}
-      {/* <img src={rawData} alt={props.descr} /> */}
-      <img src={props.src} alt={props.descr} />
+      <img src={rawData} alt={props.descr} />
+      {/* <img src={props.src} alt={props.descr} /> */}
       <div className="actions">
-        <span className="details" onClick={() => setDetails(x => !x)}><i className="fa fa-info"></i> Details</span>
-        <a className="imgDwl" href={props.src} download><i className="fa fa-download"></i> Download HD</a>
-        <div className={details ? "showDetails" : 'hide'} id={props.idx}>
+        <span className="details" onClick={() => setDetails(x => !x)}>
+          <i className="fa fa-info"></i> Details
+        </span>
+        <a className="imgDwl" href={props.src} download>
+          <i className="fa fa-download"></i> Download HD
+        </a>
+        <div className={details ? 'showDetails' : 'hide'} id={props.idx}>
           <span className="imgTitle">{props.descr}</span>
           <span className="imgOrg">Orignal:</span>
           <span className="imgStyle">Style:</span>
