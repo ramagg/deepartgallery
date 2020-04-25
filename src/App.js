a71ad4145e37d794bdafdfd0505f1dc2f50340b8
ramagg <rg1998@hotmail.es>
2020-04-25 00:37:00 -0300

    description, css

--- src/App.js
+++ src/App.js
@@ -23,18 +23,10 @@ function App() {
     const getData = async () => {
       let res = await fetch('https://ramagg.com/deepartgallery/imgs/')
       let data = await res.json()
-
-      console.log(data)
       setdataurl(parseData(data.imgs))
       setShow(true)
 
       return data
-      //   .then(response => response.json())
-      //   .then(data => {
-      //     setdataurl(grupArr(data.imgs))
-      //   })
-      // console.log('Data:', dataurl)
-      // return
     }
     getData()
   }, [])
@@ -44,6 +36,26 @@ function App() {
       <header>
         <h1>DeepArtGallery</h1>
       </header>
+      <div className="mainDescr">
+        Neural style transfer is an optimization technique used to take two
+        images—a content image and a style reference image (such as an artwork
+        by a famous painter)—and blend them together so the output image looks
+        like the content image, but “painted” in the style of the style
+        reference image. This is implemented by optimizing the output image to
+        match the content statistics of the content image and the style
+        statistics of the style reference image. These statistics are extracted
+        from the images using a convolutional network.
+        <p>
+          <a href="https://www.tensorflow.org/tutorials/generative/style_transfer">
+            Tensorflow Implementation
+          </a>
+        </p>
+        <p>
+          <a href="https://pytorch.org/tutorials/advanced/neural_style_tutorial.html?highlight=style%20transfer">
+            PyTorch Implementation
+          </a>
+        </p>
+      </div>
       <div className="artBox">
         {show
           ? dataurl.map((item, i) => {
